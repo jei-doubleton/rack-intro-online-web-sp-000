9cdc0023c6a010effa1c8657e06e1d7637dddd6e
J. Singleton <jennifer.singleton@gmail.com>
2019-03-24 16:52:02 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World #{1F60D}"
+    resp.write "Hello, World #{@hearteyes.encode('utf-8')}"
     resp.finish
   end
 
